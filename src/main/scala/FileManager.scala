cb47ecb9588003eae79147518d08f71e0bff7861
Andre Marques <andre.acm97@outlook.com>
2018-05-14 16:05:50 -0300

    Desistindo do Kick Forward

--- src/main/scala/FileManager.scala
+++ src/main/scala/FileManager.scala
@@ -1,21 +1,52 @@
+import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.ArrayBuffer
+
 class KickForward {
+
   /* Takes one input and reads all the
    * lines putting them into a list,
    * then it calls the next method.
    */
-  def readFile(Path: String, func:() => List[String]): Unit ={
-    func(scala.io.Source.fromFile(scala.io.StdIn.readLine()).getLines().toList, readStopWords())
+  def readFile(Path: String, func:() => ListBuffer[ArrayBuffer[String]]): List[String] ={
+    func(scala.io.Source.fromFile(scala.io.StdIn.readLine()).getLines().toList, removeStopWords(, ))
   }
-  // Removes unwanted characters
-  def filterChars(Text: List[String], func:() => String): List[String] ={
-    func(Text.foreach(_.filterNot("""!"#$%&'()*+,-./:;<>=?@[]\_{}|""".toSet)), proxima)//Usando aspas triplas para incluir as aspas e aspas simples na string
+
+  def splitStrings(Text: List[String], func:() => ListBuffer[ArrayBuffer[String]]):ListBuffer[ArrayBuffer[String]]={
+    val separateString = new ListBuffer[ArrayBuffer[String]]()
+    for(phrases <- Text)separateString += phrases.split(" ").to[ArrayBuffer]
+    func(separateString, func)
   }
   // Removes unwanted words
-  def removeStopWords(Text: List[String], func:() => List[String]): List[String] ={
-    func(Text.foreach(_.filterNot(scala.io.Source.fromFile("StopWords.txt").getLines().toList.toSet)), proxima)
+  def removeStopWords(separatedString: ListBuffer[ArrayBuffer[String]], func:() => String): ListBuffer[ArrayBuffer[String]] ={//Como fazer puramente funcional?
+    val filtered = new ListBuffer[ArrayBuffer[String]]()
+    val auxList = separatedString.clone()
+    val stopWords = scala.io.Source.fromFile("stopWords.txt").getLines().toList
+    for(arrays <- auxList; stop <- stopWords) arrays.contains(stop) match{
+      case true => {
+        arrays -= stop
+        removeStopWords(auxList, prox)
+      }
+      case false => filtered += arrays
+    }
   }
-  def wordShift(Text: List[String], func:() => List[String]): List[String] ={
-    val target = scala.io.StdIn.readInt()
-    
+
+
+
+  def printWords(cleanedString: ListBuffer[ArrayBuffer[String]], func:() => Unit) ={//Como fazer sem usar o for como um while
+    var aux:String= ""
+    val auxString = cleanedString.clone
+    for(words <- auxString){
+      for(n <- words.indices){
+        aux = words(0)
+        for(i <- 0 to words.length - 2){
+          words.update(i, words(i+1))
+        }
+        words.update(words.size-1, aux)
+        print(" | "+words(0)+" | ")
+        for(j <- 1 until words.length){ print(words(j)+" ")}
+        print("\n")
+      }
+    }
   }
+
 }
